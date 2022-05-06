96ebbcd2aa527e5c1dea4cd391d6cc80e487c557
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2022-05-06 22:25:10 +0800

    vue3响应式ref

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
+<template>
+  <h1>{{msg}}</h1>
+
+  <!-- 方法里修改数据 -->
+  <h1>{{revseMsg(msg)}}</h1>
+
+  <!-- 动态修改数据 -->
+  <button @click="setMsg">点击修改</button>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
-</script>
+import { ref } from 'vue'
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+/**
+ * 这样写不是响应式
+ */
+// let msg = '2022/5/6'
+let msg = ref('2022/5/6'); //创建响应式对象
+
+function revseMsg(val) {
+  /**
+   * 转成数组 ---》 数组颠倒的方法 ---》转成字符串
+   */
+  return val.split('').reverse().join('')
+}
+
+function setMsg() {
+  /**
+   * 
+   * 这样写不是响应式
+   * 
+   * 当前数据已改，但是视图没有进行修改
+   */
+  // msg = '精灵旅社收官之作不好玩！！！'
+  msg.value = '精灵旅社收官之作不好玩！！！' //.value 才可以修改其值
+  console.log(msg, '---msg');
+}
+</script>
 
 <style>
 #app {
